d29484d1e2bdc99c27fb4d4d8c4110079fc15aad
Prasad Zoman <prasadzoman15@gmail.com>
2025-05-24 13:11:03 +0530

    Deployment issue solved

--- app.js
+++ app.js
@@ -66,9 +66,9 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")))
-// app.get('/', (req, res) => {
-//     res.send('server working');
-// })
+ app.get('/', (req, res) => {
+     res.redirect("/listings");
+ })
 
 app.use("/listings",listings);
 //reviews
